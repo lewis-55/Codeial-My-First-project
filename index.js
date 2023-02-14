fedd4b796fa6b2fe988d8d83128dbd4264dda234
Lewis <luizu05@gmail.com>
2023-02-14 19:11:59 +0530

    Adding css files init

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8000;
  
 const expressLayouts = require('express-ejs-layouts');
+app.use(express.static('./assets'));
 app.use(expressLayouts);
 // use express router
 app.use('/',require('./routes/index'));
