a55d347303482f4182618cdf15b1faac6ab80aa4
Lewis <luizu05@gmail.com>
2023-02-14 20:16:51 +0530

    Added static files

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 app.use(express.static('./assets'));
 app.use(expressLayouts);
+
+//extract styles and scripts from the sub pages into the layouts
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 // use express router
 app.use('/',require('./routes/index'));
 
