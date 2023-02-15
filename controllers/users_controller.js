8ce1ea272f792b9bc2a5f677a974479cf545b554
Lewis <luizu05@gmail.com>
2023-02-15 23:09:20 +0530

    Adding sign up and sign in form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,9 @@
-   module.exports.profile = function(req,res){
+  const { response } = require('express');
+const user = require('../models/user');
+const User = require('../models/user');
+
+
+module.exports.profile = function(req,res){
        return res.render('users',{
         title:"Users"
        });
@@ -18,8 +23,23 @@
 
 // get the sign up data
 module.exports.create = function(req,res){
-  // To Do later
-};
+  if(req.body.password != req.body.confirm_password){
+    return res.redirect('back');
+  }
+  user.findOne({email : req.body.email},function(err,user){
+    if(err){console.log('erroe in finding user in singing up');return}
+
+    if(!user){
+        user.create(res.body,function(err,user){
+            if(err){console.log('erroe in finding user in singing up');return}
+
+            return res.redirect('/user/sign-in');
+        })
+    } else {
+        return res.redirect('back');
+    }
+  });
+}
 
 // sing in and create the sessions for the user
 module.exports.createSession = function(req, res){
