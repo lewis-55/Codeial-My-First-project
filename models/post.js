d66e9ecd80cc5f54f71b179e70fe9bbd1a004b13
Lewis <luizu05@gmail.com>
2023-02-17 20:40:49 +0530

    Adding Schema

---
+++ models/post.js
@@ -0,0 +1,18 @@
+    const mongoose = require('mongoose');
+    
+    
+    const userSchema = new mongoose.Schema({
+        content : {
+            type : string ,
+            require : true
+        },
+        user : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'user'
+        }
+    },{
+        timestamps : true
+    });
+
+    const post = mongoose.model('post',postSchema);
+    model.exports = post;
